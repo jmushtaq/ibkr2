8d085e3d002e219b5baed9831f53ae3030cde845
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2026-03-20 22:47:43 +0800

    fixed filter - as_of date was resetting

--- markets/filters.py
+++ markets/filters.py
@@ -68,7 +68,7 @@ class SymbolMetricsFilter(django_filters.FilterSet):
         widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'To'})
     )
 
-    # Percentage change filters
+    # Historical Percentage change filters
     change_1d_min = django_filters.NumberFilter(
         field_name='change_1d',
         lookup_expr='gte',
@@ -125,6 +125,190 @@ class SymbolMetricsFilter(django_filters.FilterSet):
         widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
     )
 
+    # Forward-looking filters
+    fwd_change_1d_min = django_filters.NumberFilter(
+        field_name='fwd_change_1d',
+        lookup_expr='gte',
+        label='Fwd 1D % Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_change_1d_max = django_filters.NumberFilter(
+        field_name='fwd_change_1d',
+        lookup_expr='lte',
+        label='Fwd 1D % Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_change_1w_min = django_filters.NumberFilter(
+        field_name='fwd_change_1w',
+        lookup_expr='gte',
+        label='Fwd 1W % Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_change_1w_max = django_filters.NumberFilter(
+        field_name='fwd_change_1w',
+        lookup_expr='lte',
+        label='Fwd 1W % Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_change_2w_min = django_filters.NumberFilter(
+        field_name='fwd_change_2w',
+        lookup_expr='gte',
+        label='Fwd 2W % Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_change_2w_max = django_filters.NumberFilter(
+        field_name='fwd_change_2w',
+        lookup_expr='lte',
+        label='Fwd 2W % Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_change_1m_min = django_filters.NumberFilter(
+        field_name='fwd_change_1m',
+        lookup_expr='gte',
+        label='Fwd 1M % Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_change_1m_max = django_filters.NumberFilter(
+        field_name='fwd_change_1m',
+        lookup_expr='lte',
+        label='Fwd 1M % Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_change_3m_min = django_filters.NumberFilter(
+        field_name='fwd_change_3m',
+        lookup_expr='gte',
+        label='Fwd 3M % Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_change_3m_max = django_filters.NumberFilter(
+        field_name='fwd_change_3m',
+        lookup_expr='lte',
+        label='Fwd 3M % Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_change_6m_min = django_filters.NumberFilter(
+        field_name='fwd_change_6m',
+        lookup_expr='gte',
+        label='Fwd 6M % Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_change_6m_max = django_filters.NumberFilter(
+        field_name='fwd_change_6m',
+        lookup_expr='lte',
+        label='Fwd 6M % Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_change_1y_min = django_filters.NumberFilter(
+        field_name='fwd_change_1y',
+        lookup_expr='gte',
+        label='Fwd 1Y % Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_change_1y_max = django_filters.NumberFilter(
+        field_name='fwd_change_1y',
+        lookup_expr='lte',
+        label='Fwd 1Y % Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    # Risk metrics filters
+    fwd_volatility_1m_min = django_filters.NumberFilter(
+        field_name='fwd_volatility_1m',
+        lookup_expr='gte',
+        label='Volatility 1M Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_volatility_1m_max = django_filters.NumberFilter(
+        field_name='fwd_volatility_1m',
+        lookup_expr='lte',
+        label='Volatility 1M Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_volatility_3m_min = django_filters.NumberFilter(
+        field_name='fwd_volatility_3m',
+        lookup_expr='gte',
+        label='Volatility 3M Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_volatility_3m_max = django_filters.NumberFilter(
+        field_name='fwd_volatility_3m',
+        lookup_expr='lte',
+        label='Volatility 3M Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_volatility_6m_min = django_filters.NumberFilter(
+        field_name='fwd_volatility_6m',
+        lookup_expr='gte',
+        label='Volatility 6M Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_volatility_6m_max = django_filters.NumberFilter(
+        field_name='fwd_volatility_6m',
+        lookup_expr='lte',
+        label='Volatility 6M Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_sharpe_ratio_min = django_filters.NumberFilter(
+        field_name='fwd_sharpe_ratio',
+        lookup_expr='gte',
+        label='Sharpe Ratio Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min'})
+    )
+
+    fwd_sharpe_ratio_max = django_filters.NumberFilter(
+        field_name='fwd_sharpe_ratio',
+        lookup_expr='lte',
+        label='Sharpe Ratio Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max'})
+    )
+
+    fwd_max_drawdown_min = django_filters.NumberFilter(
+        field_name='fwd_max_drawdown',
+        lookup_expr='gte',
+        label='Max Drawdown Min',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min %'})
+    )
+
+    fwd_max_drawdown_max = django_filters.NumberFilter(
+        field_name='fwd_max_drawdown',
+        lookup_expr='lte',
+        label='Max Drawdown Max',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max %'})
+    )
+
+    fwd_drawdown_duration_min = django_filters.NumberFilter(
+        field_name='fwd_drawdown_duration',
+        lookup_expr='gte',
+        label='Drawdown Duration Min (days)',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Min days'})
+    )
+
+    fwd_drawdown_duration_max = django_filters.NumberFilter(
+        field_name='fwd_drawdown_duration',
+        lookup_expr='lte',
+        label='Drawdown Duration Max (days)',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max days'})
+    )
+
     # Market cap filter
     market_cap_min = django_filters.NumberFilter(
         field_name='symbol__market_cap',
@@ -161,6 +345,15 @@ class SymbolMetricsFilter(django_filters.FilterSet):
             'search', 'sector', 'industry', 'year_from', 'year_to',
             'change_1d_min', 'change_1d_max', 'change_1w_min', 'change_1w_max',
             'change_1m_min', 'change_1m_max', 'change_1y_min', 'change_1y_max',
+            'fwd_change_1d_min', 'fwd_change_1d_max', 'fwd_change_1w_min', 'fwd_change_1w_max',
+            'fwd_change_2w_min', 'fwd_change_2w_max', 'fwd_change_1m_min', 'fwd_change_1m_max',
+            'fwd_change_3m_min', 'fwd_change_3m_max', 'fwd_change_6m_min', 'fwd_change_6m_max',
+            'fwd_change_1y_min', 'fwd_change_1y_max',
+            'fwd_volatility_1m_min', 'fwd_volatility_1m_max', 'fwd_volatility_3m_min', 'fwd_volatility_3m_max',
+            'fwd_volatility_6m_min', 'fwd_volatility_6m_max',
+            'fwd_sharpe_ratio_min', 'fwd_sharpe_ratio_max',
+            'fwd_max_drawdown_min', 'fwd_max_drawdown_max',
+            'fwd_drawdown_duration_min', 'fwd_drawdown_duration_max',
             'market_cap_min', 'market_cap_max', 'price_min', 'price_max'
         ]
 
